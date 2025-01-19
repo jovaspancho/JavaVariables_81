b4e8d8791f23513d4c90aa5237e7c80586f12b1d
JesseAinsworth <al222010228@gmail.com>
2025-01-18 23:14:36 -0600

    inicio

---
+++ src/main/java/Jovas/utvt/clase1/HelloWorld.java
@@ -0,0 +1,7 @@
+package edu.utvt.clase1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!");
+    }
+}
---
+++ src/main/java/Jovas/utvt/clase1/variables.java
@@ -0,0 +1,18 @@
+package edu.utvt.clase1;
+
+public class variables {
+    public static void main(String[] args) {
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+        int initValue;
+        initValue = 100;
+        System.out.println(initValue);
+
+        long hugeValue = 1000000L;
+        System.out.println(hugeValue);
+
+        float floatVar = 3.14F;
+        System.out.println(floatVar);
+    }
+}
